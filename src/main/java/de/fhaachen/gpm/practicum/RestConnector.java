cc71f18f2fc4abd831e9cd51ce77a14a32757aaf
Manuel Bareiss <manuel.bareiss92@gmail.com>
2017-11-28 13:21:27 +0100

    Erste Schritte Rest-Connector

--- src/main/java/de/fhaachen/gpm/practicum/RestConnector.java
+++ src/main/java/de/fhaachen/gpm/practicum/RestConnector.java
@@ -1,5 +1,8 @@
 package de.fhaachen.gpm.practicum;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
@@ -13,13 +16,30 @@ public class RestConnector implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
 
-        //url = new URL((String) delegateExecution.getVariable("url"));
-        url = new URL("http://localhost:8000/insertThesis?student_id=1&title=Test&supervisor=Fassbender");
-        //String theMethod = (String) delegateExecution.getVariable("method");
-        String theMethod = "PUT";
-        HttpURLConnection theConnection = (HttpURLConnection)  url.openConnection();
-        theConnection.setRequestMethod(theMethod);
-        theConnection.connect();
+        try {
+            Client client = Client.create();
+
+            WebResource webResource = client
+                    .resource("http://localhost:8000/insertThesis?student_id=1&title=Test&supervisor=Fassbender");
+
+            ClientResponse response = webResource.accept("application/json")
+                    .put(ClientResponse.class);
+
+            if (response.getStatus() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatus());
+            }
+
+            String output = response.getEntity(String.class);
+
+            System.out.println("Output from Server .... \n");
+            System.out.println(output);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
 
     }
 }

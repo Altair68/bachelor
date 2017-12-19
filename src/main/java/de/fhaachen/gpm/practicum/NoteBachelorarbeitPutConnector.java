5a817bfafbea171eeead69ebbf18e4ed1a064fd5
Manuel Bareiss <manuel.bareiss92@gmail.com>
2017-12-19 12:20:51 +0100

    get strawpoll

---
+++ src/main/java/de/fhaachen/gpm/practicum/NoteBachelorarbeitPutConnector.java
@@ -0,0 +1,24 @@
+package de.fhaachen.gpm.practicum;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class NoteBachelorarbeitPutConnector extends RestConnectorBase {
+
+    @Override
+    protected ClientResponse execMethod(WebResource aResource) {
+        ClientResponse theResponse = aResource.accept("application/json")
+                .put(ClientResponse.class);
+
+        return theResponse;
+    }
+
+    @Override
+    protected String getUrl() {
+        return String.format("http://localhost:8000/insertBach?student_id=%s&grade=%s",
+                getDelegateExecution().getVariable("StudentId"),
+                getDelegateExecution().getVariable("NoteBachelorarbeit"));
+    }
+}

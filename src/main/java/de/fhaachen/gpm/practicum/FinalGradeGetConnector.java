518ea7ed629166f8f466b984e90c227d6f579277
Manuel Bareiss <manuel.bareiss92@gmail.com>
2017-12-19 13:03:11 +0100

    get strawpoll

---
+++ src/main/java/de/fhaachen/gpm/practicum/FinalGradeGetConnector.java
@@ -0,0 +1,25 @@
+package de.fhaachen.gpm.practicum;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class FinalGradeGetConnector extends RestConnectorBase {
+    @Override
+    protected ClientResponse execMethod(WebResource aResource) {
+        ClientResponse theResponse = aResource.accept("application/json")
+                .get(ClientResponse.class);
+
+        final String theResult = theResponse.getEntity(String.class);
+
+        getDelegateExecution().setVariable("FinalGrade", theResult);
+        return theResponse;
+    }
+
+    @Override
+    protected String getUrl() {
+        return String.format("http://localhost:8000/insertBach?student_id=%s",
+                getDelegateExecution().getVariable("StudentId"));
+    }
+}
